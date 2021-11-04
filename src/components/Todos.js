0e1e0f3dae2d823cd192b3c2ef64f7ffb2ff7b6c
최준원 <cjw1102@naver.com>
2021-11-04 16:16:24 +0900

    React redux

---
+++ src/components/Todos.js
@@ -0,0 +1,50 @@
+const TodoItems = ({ todo, onToggle, onRemove }) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? "line-through" : "none" }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
+    </div>
+  );
+};
+
+const Todos = ({
+  input,
+  todos,
+  onChangeInput,
+  onInsert,
+  onToggle,
+  onRemove,
+}) => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(input);
+    onChangeInput(""); // 등록 후 input 초기화
+  };
+  const onChange = (e) => onChangeInput(e.target.value);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={input} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      {todos.map((todo) => (
+        <TodoItems
+          todo={todo}
+          key={todo.id}
+          onToggle={onToggle}
+          onRemove={onRemove}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Todos;
